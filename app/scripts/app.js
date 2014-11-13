57094e71a3f9db6b472772be746596577888fa60
Tobias Haaß <tobias.haass@andrena.de>
2014-11-13 11:35:52 +0100

    Projekt mit 'yo angular' aufgesetzt

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name validationDirectiveApp
+ * @description
+ * # validationDirectiveApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('validationDirectiveApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name validationDirectiveApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the validationDirectiveApp
+ */
+angular.module('validationDirectiveApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
